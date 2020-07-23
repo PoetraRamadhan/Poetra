ff9d9ade8a84bb3cf8ca361c53674c97b819d2c3
ShardNsf <57884680+ShardNsf@users.noreply.github.com>
2020-07-23 09:24:58 +0700

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -1,11 +1,11 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.guild.members.cache.find(x => x.user.username === args.slice(0).join(" ") || x.user.username === args[0]) || message.author;
+    let member = message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.author;
     let avatar = new Discord.MessageEmbed()
-    .setTitle(`${member.tag}`)
-    .setDescription(`\`\`Name: ${member.username}\nID: ${member.id}\`\``)
-    .setImage(member.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 }))
+    .setTitle(`${member.user.tag}`)
+    .setDescription(`\`\`Name: ${member.user.username}\nID: ${member.id}\`\``)
+    .setImage(member.user.displayAvatarURL({ dynamic: true, format: 'png', size: 1024 }))
     message.channel.send(avatar)
 }
 

9ade79c49a2024f8f594facb0f703e70cf747136
ShardNsf <57884680+ShardNsf@users.noreply.github.com>
2020-08-02 10:36:07 +0700

    updates

--- commands/ban.js
+++ commands/ban.js
@@ -2,25 +2,25 @@ const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
     if(!message.member.hasPermission("BAN_MEMBERS")) {
-        return message.reply("You do not have the permission to use this command");
+        return message.reply("<:Fail:739301997575929976>|You do not have the permission to use this command");
     }
 
     let target = message.mentions.members.first() || message.guild.members.cache.get(args[0])
 
     if(!target) {
-        return message.reply("Coulnd't find User, Please provide a User or UserID");
+        return message.reply("<:Fail:739301997575929976>|Coulnd't find User, Please provide a User or UserID");
     }
 
     if(target.id === message.author.id) {
-        return message.reply("You cannot ban yourself.");
+        return message.reply("<:Fail:739301997575929976>|You cannot ban yourself.");
     }
 
     if(target.id === message.guild.owner.id) {
-        return message.reply("You cannot ban the guild owner.");
+        return message.reply("<:Fail:739301997575929976>|You cannot ban the guild owner.");
     }
 
     if(!target.bannable) {
-        return message.reply("Coulnd't ban this member. Maybe his/her role is higher than me?")
+        return message.reply("<:Fail:739301997575929976>|Coulnd't ban this member. Maybe his/her role is higher than me?")
     }
 
 
@@ -29,10 +29,7 @@ module.exports.run = async (client, message, args) => {
 
     try {
         target.ban(reason);
-        let embed = new Discord.MessageEmbed()
-        .setAuthor(`Successfully banned ${target.user.tag}`, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQqiK692F33PbCxxcrJjTaHmIk68heDGtXbJA&usqp=CAU")
-        .setColor("BLUE")
-        message.channel.send(embed).then(m => m.delete({ timeout: 20000 }))
+        message.channel.send(`<:Success:739301940705624135>|Successfuly Banned ${target.user.tag}`).then(m => m.delete({ timeout: 20000 }))
     } catch (err) {
         if(err) console.log(err);
     }
--- commands/unban.js
+++ commands/unban.js
@@ -1,23 +1,19 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (client, message, args) => {
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("You do nut have the permission to use this command.")
+    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("<:Fail:739301997575929976>|You do nut have the permission to use this command.")
 
     let bannedUser = await client.users.fetch(args[0])
-    if(!bannedUser) return message.channel.send("Please provide a user to unbanned!")
+    if(!bannedUser) return message.channel.send("<:Fail:739301997575929976>|Please provide a user to unbanned!")
 
     let reason = args.slice(1).join(" ")
     if(!reason) reason = "No reason"
 
     try{
-        let embed = new Discord.MessageEmbed()
-        .setAuthor(`Successfully unBan ${bannedUser.tag}`, "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQqiK692F33PbCxxcrJjTaHmIk68heDGtXbJA&usqp=CAU")
-        .setColor("BLUE")
         message.guild.members.unban(bannedUser, {reason: reason})
-        message.channel.send(embed).then(m => m.delete({ timeout: 20000 }))
+        message.channel.send(`Successfully Unban ${bannedUser.user.tag}`).then(m => m.delete({ timeout: 20000 }))
     } catch (error) {
         console.log(error)
-        message.channel.send(`ERROR:\n\`\`\`${error}\`\`\``)
     }
 }
 
